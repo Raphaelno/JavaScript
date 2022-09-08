663624bc355a77a8c760c8b47fa5ae595739a733
Raphaelno <raphaelporque@gmail.com>
2022-09-08 12:00:31 -0300

    JavaScript

---
+++ Aula 2/2/index.js
@@ -0,0 +1,37 @@
+var variavel = 45 // inteiro                        "variavel nesse caso é o nome da variavel"
+var variavel = 49.5 // real
+var variavel = "Programadores cariocas"
+var variavel = "A"
+var variavel = false // logico
+let // é mais consistente 
+var variavel // liberdade maior
+
+const pi = 3.1416; // não vai ser subscrevido sempre vai ser a mesma coisa "3.1416 nesse caso" pi nesse é o nome da const
+
+/* operadores aritimeticos
+{entre chaves é o escopo, o que vai ter de ação}
+(+ , - , *, / %)
+no portugol usavamos "," para contatena e agora usamos o "+"
+% se chama módulo*/
+
+/* operadores relacionais
+(==, >=, <=, >, <, !=)
+== igual
+>= maior ou igual
+<= menor ou igual
+> maior
+< menor
+!= diferente
+*/
+
+/* operadores logicos
+ e ou vão ser substituido por && ||
+ e(&&)
+ ou(||)*/
+
+ /*incremento e decremento
+ ++ adiciona um numero
+ -- tira um numero 
+ ex: 1++, a cada ciclo o numero 1 vai subir para 2 e depois para 3 etc....
+ */
+
