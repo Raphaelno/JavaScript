11eb6affd0809dc4e2f3802e932b85d94261d7dc
Raphaelno <raphaelporque@gmail.com>
2022-09-28 11:45:27 -0300

    Atualização

---
+++ Aula 2_DATA08_09_22/Diabete/diabete.js
@@ -0,0 +1,24 @@
+let P, N, A, D, B
+alert("Ola. Por favor preencha o formulário abaixo.")
+N=prompt("Nome: ")
+P=prompt("Peso: ")
+A=prompt("Altura: ")
+D=prompt("Data de internação: ")
+B=prompt("Você é diabético? ")
+alert("Obrigado pelo teu tempo. A seguir mostrarei todos os teus dados!")
+alert("\nTudo certo?" + "\nNome: " + N + "\nPeso: " + P + "\nAltura: " + A + "\nData de internação: " + D + "\nDiabético: " + B)
+ 
+/*
+P = peso
+N = nome
+A = altura
+D = data de internação
+B = Diabético*/
+ 
+ 
+/*
+Crie um programa de saúde que trabalhe com
+variáveis que recebam dados de peso, nome,
+altura, data de internação e uma variável
+que receba o dado se o paciente é diabético
+ou não.*/
